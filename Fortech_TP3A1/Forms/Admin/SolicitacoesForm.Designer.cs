98c2e69fab7744d3dd2d6898445f84ca189b4cca
Luan Coelho de Souza <luancoelho@unitins.br>
2022-11-26 00:36:40 -0300

    Adicionado arquivos não versionados

---
+++ Fortech_TP3A1/Forms/Admin/SolicitacoesForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Fortech_TP3A1.Forms.Admin
+{
+    partial class SolicitacoesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fortechDataSet5 = new Fortech_TP3A1.fortechDataSet5();
+            this.solicitacaoServicoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.solicitacaoServicoTableAdapter = new Fortech_TP3A1.fortechDataSet5TableAdapters.SolicitacaoServicoTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.detalhesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuarioIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eletronicoIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipoServicoidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbGerencia = new System.Windows.Forms.Label();
+            this.btVoltar = new System.Windows.Forms.Button();
+            this.btEncerrarSessao = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fortechDataSet5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solicitacaoServicoBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.detalhesDataGridViewTextBoxColumn,
+            this.valorDataGridViewTextBoxColumn,
+            this.usuarioIdDataGridViewTextBoxColumn,
+            this.eletronicoIdDataGridViewTextBoxColumn,
+            this.tipoServicoidDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.solicitacaoServicoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(744, 282);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fortechDataSet5
+            // 
+            this.fortechDataSet5.DataSetName = "fortechDataSet5";
+            this.fortechDataSet5.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // solicitacaoServicoBindingSource
+            // 
+            this.solicitacaoServicoBindingSource.DataMember = "SolicitacaoServico";
+            this.solicitacaoServicoBindingSource.DataSource = this.fortechDataSet5;
+            // 
+            // solicitacaoServicoTableAdapter
+            // 
+            this.solicitacaoServicoTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            this.nomeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // detalhesDataGridViewTextBoxColumn
+            // 
+            this.detalhesDataGridViewTextBoxColumn.DataPropertyName = "detalhes";
+            this.detalhesDataGridViewTextBoxColumn.HeaderText = "detalhes";
+            this.detalhesDataGridViewTextBoxColumn.Name = "detalhesDataGridViewTextBoxColumn";
+            this.detalhesDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // valorDataGridViewTextBoxColumn
+            // 
+            this.valorDataGridViewTextBoxColumn.DataPropertyName = "valor";
+            this.valorDataGridViewTextBoxColumn.HeaderText = "valor";
+            this.valorDataGridViewTextBoxColumn.Name = "valorDataGridViewTextBoxColumn";
+            this.valorDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // usuarioIdDataGridViewTextBoxColumn
+            // 
+            this.usuarioIdDataGridViewTextBoxColumn.DataPropertyName = "Usuario_Id";
+            this.usuarioIdDataGridViewTextBoxColumn.HeaderText = "Usuario_Id";
+            this.usuarioIdDataGridViewTextBoxColumn.Name = "usuarioIdDataGridViewTextBoxColumn";
+            this.usuarioIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // eletronicoIdDataGridViewTextBoxColumn
+            // 
+            this.eletronicoIdDataGridViewTextBoxColumn.DataPropertyName = "eletronico_Id";
+            this.eletronicoIdDataGridViewTextBoxColumn.HeaderText = "eletronico_Id";
+            this.eletronicoIdDataGridViewTextBoxColumn.Name = "eletronicoIdDataGridViewTextBoxColumn";
+            this.eletronicoIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // tipoServicoidDataGridViewTextBoxColumn
+            // 
+            this.tipoServicoidDataGridViewTextBoxColumn.DataPropertyName = "tipoServico_id";
+            this.tipoServicoidDataGridViewTextBoxColumn.HeaderText = "tipoServico_id";
+            this.tipoServicoidDataGridViewTextBoxColumn.Name = "tipoServicoidDataGridViewTextBoxColumn";
+            this.tipoServicoidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.btVoltar);
+            this.panel1.Controls.Add(this.btEncerrarSessao);
+            this.panel1.Controls.Add(this.lbGerencia);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(769, 75);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbGerencia
+            // 
+            this.lbGerencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGerencia.ForeColor = System.Drawing.Color.White;
+            this.lbGerencia.Location = new System.Drawing.Point(31, 22);
+            this.lbGerencia.Name = "lbGerencia";
+            this.lbGerencia.Size = new System.Drawing.Size(272, 29);
+            this.lbGerencia.TabIndex = 1;
+            this.lbGerencia.Text = "Solicitações de serviço";
+            // 
+            // btVoltar
+            // 
+            this.btVoltar.BackColor = System.Drawing.Color.DarkGray;
+            this.btVoltar.FlatAppearance.BorderSize = 0;
+            this.btVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btVoltar.ForeColor = System.Drawing.Color.White;
+            this.btVoltar.Location = new System.Drawing.Point(553, 19);
+            this.btVoltar.Name = "btVoltar";
+            this.btVoltar.Size = new System.Drawing.Size(76, 33);
+            this.btVoltar.TabIndex = 5;
+            this.btVoltar.Text = "Voltar";
+            this.btVoltar.UseVisualStyleBackColor = false;
+            this.btVoltar.Click += new System.EventHandler(this.btVoltar_Click);
+            // 
+            // btEncerrarSessao
+            // 
+            this.btEncerrarSessao.BackColor = System.Drawing.Color.Red;
+            this.btEncerrarSessao.FlatAppearance.BorderSize = 0;
+            this.btEncerrarSessao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btEncerrarSessao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEncerrarSessao.ForeColor = System.Drawing.Color.White;
+            this.btEncerrarSessao.Location = new System.Drawing.Point(635, 22);
+            this.btEncerrarSessao.Name = "btEncerrarSessao";
+            this.btEncerrarSessao.Size = new System.Drawing.Size(121, 27);
+            this.btEncerrarSessao.TabIndex = 6;
+            this.btEncerrarSessao.Text = "Encerrar Sessão";
+            this.btEncerrarSessao.UseVisualStyleBackColor = false;
+            this.btEncerrarSessao.Click += new System.EventHandler(this.btEncerrarSessao_Click);
+            // 
+            // SolicitacoesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 414);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SolicitacoesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Solicitações";
+            this.Load += new System.EventHandler(this.SolicitacoesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fortechDataSet5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.solicitacaoServicoBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private fortechDataSet5 fortechDataSet5;
+        private System.Windows.Forms.BindingSource solicitacaoServicoBindingSource;
+        private fortechDataSet5TableAdapters.SolicitacaoServicoTableAdapter solicitacaoServicoTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn detalhesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuarioIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eletronicoIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipoServicoidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbGerencia;
+        private System.Windows.Forms.Button btVoltar;
+        private System.Windows.Forms.Button btEncerrarSessao;
+    }
+}
---
+++ Fortech_TP3A1/Forms/Migrador.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.ComponentModel;
+
+namespace Fortech_TP3A1.Forms
+{
+    partial class Migrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.btAvancar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.AutoSize = true;
+            this.lbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenha.Location = new System.Drawing.Point(42, 101);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(70, 24);
+            this.lbSenha.TabIndex = 71;
+            this.lbSenha.Text = "Coluna";
+            // 
+            // btAvancar
+            // 
+            this.btAvancar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btAvancar.FlatAppearance.BorderSize = 0;
+            this.btAvancar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAvancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAvancar.ForeColor = System.Drawing.Color.White;
+            this.btAvancar.Location = new System.Drawing.Point(413, 52);
+            this.btAvancar.Name = "btAvancar";
+            this.btAvancar.Size = new System.Drawing.Size(141, 33);
+            this.btAvancar.TabIndex = 72;
+            this.btAvancar.Text = "Uploud da planilha";
+            this.btAvancar.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] { "A", "B", "C", "D", "E", "F", "G" });
+            this.comboBox1.Location = new System.Drawing.Point(42, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 39);
+            this.comboBox1.TabIndex = 73;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] { "Nome", "CPF", "RG", "Data Nascimento" });
+            this.comboBox2.Location = new System.Drawing.Point(171, 128);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 39);
+            this.comboBox2.TabIndex = 74;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 24);
+            this.label1.TabIndex = 75;
+            this.label1.Text = "Tipo de informação";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(350, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 24);
+            this.label2.TabIndex = 77;
+            this.label2.Text = "Formato da informação";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] { "Texto", "Numérico" });
+            this.comboBox3.Location = new System.Drawing.Point(353, 128);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(201, 39);
+            this.comboBox3.TabIndex = 76;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(42, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 33);
+            this.button1.TabIndex = 78;
+            this.button1.Text = "Adicionar coluna";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(560, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 33);
+            this.button2.TabIndex = 79;
+            this.button2.Text = "Adicionar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Migrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 224);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btAvancar);
+            this.Controls.Add(this.lbSenha);
+            this.Name = "Migrador";
+            this.Text = "Migrador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+
+        private System.Windows.Forms.Button btAvancar;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Label lbSenha;
+
+        #endregion
+    }
+}
